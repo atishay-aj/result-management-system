c16d562a238aee14361b7221eb19984ea3ddffdc
atishayjain <atishay.jain@nagarro.com>
2022-12-05 17:33:57 +0530

    azure

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 //express app
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const mongoose = require('mongoose'); //to connect to mongodb
 
 //connect to mongodb and listen to requests
